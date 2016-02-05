10fd186fcde2765df62f612dfee21965d2968e28
Pallav Kulshreshtha <pallav.kul@gmail.com>
2016-02-05 13:14:19 +0530

    Fixed directory viewer component

--- app/components/directory-viewer.js
+++ app/components/directory-viewer.js
@@ -15,7 +15,18 @@ export default Ember.Component.extend({
          accepts: 'application/json',
          success: function(records) {
 
-             if(parent !== null){ //This will run only after first level is generated
+             var dirRecords = {};
+             dirRecords['files'] = [];
+
+             // filtering only the directory records.
+             for(var i=0; i < records['files'].length; i++ ){
+                  if(records.files[i].isDirectory){
+                      dirRecords['files'].push(records['files'][i]);
+                  }
+             }
+
+             if(parent !== null){
+               //This will run only after first level is generated
                var temp = '';
                var arr =  parent.split('');
                for(var i=0; i < arr.length; i++){
@@ -24,22 +35,23 @@ export default Ember.Component.extend({
                eval('result' + temp  + '= []');
              }
 
-             for(var i = 0; i < records.files.length; i++ ){
-                 if(records.files[i].isDirectory){ //check for only the folder/directory
-                      var tempJson = {}
-                      var recordsArr = records.files[i].path.split('/');
+             var dirRecordsLength = dirRecords.files.length;
+             for(var i = 0; i < dirRecordsLength; i++ ) {
+                var tempJson = {}
+                var recordsArr = dirRecords.files[i].path.split('/');
 
-                      tempJson['text'] = recordsArr[recordsArr.length-1]; // We want to show only the name of the directory not the completepath
-                      tempJson['completepath'] = records.files[i].path;
+                tempJson['text'] = recordsArr[recordsArr.length-1];  // setting text property to JSON
+                tempJson['completepath'] = dirRecords.files[i].path; // setting completepath property to JSON
 
-                      if(parent !== null){ //This will run only after first level is generated
-                       tempJson['parent'] =  parent.toString() + i.toString();
-                       eval('result' + temp).push(tempJson);
-                      } else{ //This will run only once when first level is generated
-                       tempJson['parent'] = i.toString();
-                       result.push(tempJson);
-                      }
-                 }
+                if(parent !== null){
+                   //This will run only after first level is generated
+                   tempJson['parent'] =  parent.toString() + i.toString();
+                   eval('result' + temp).push(tempJson);
+                } else {
+                   //This will run only once when first level is generated
+                   tempJson['parent'] = i.toString();
+                   result.push(tempJson);
+                }
              }
 
               $('#tree').treeview( {
@@ -52,7 +64,7 @@ export default Ember.Component.extend({
                 emptyIcon: "fa fa-folder",
                 onNodeSelected: function(event, data) {
 
-                   //We have to track the scrollTop of the container because resets it to 0 after rendering of json tree.
+                   //Tracks scrollTop of the container(tree) because it gets reset to 0 after rendering the json tree.
                    var $treeElmnt = $(event.target);
                    var $treeElmntScrollHeight = $treeElmnt.scrollTop();
 
@@ -60,13 +72,14 @@ export default Ember.Component.extend({
 
                    init(dirUrl, data.parent);
 
-                   /* TODO :: Use Ember promise for this and exceute the below fun only for successful callback.  */
+
                    setTimeout(function(){
+                     /* TODO :: Use Ember promise for this and exceute the below lines only after geeting successful callback.  */
 
                       $('#tree').treeview('expandNode', [ data.nodeId, { silent: true } ]);
                       $('#tree').treeview('revealNode', [ data.nodeId, { silent: true } ]);
                       $('#tree').treeview('selectNode', [ data.nodeId, { silent: true } ]);
-                      
+
                       $treeElmnt.scrollTop($treeElmntScrollHeight);
 
                       _self.set('selectedFolderPath', data.completepath );
@@ -84,25 +97,11 @@ export default Ember.Component.extend({
   };
 
   init(this.initUrl , null);
-  //$('#tree').treeview( {data: getTree()});
-  //$('#tree').treeview('collapseAll', { silent: true });
+
   },
   actions: {
     setSelectedFolder: function(path) {
       this.parentView.set('selectionName', path);
-    },
-    expandParent: function(nodeId){
-        if(nodeId){
-          var itsParent = $('#tree').treeview('getParent', nodeId);
-          if(itsParent.state){
-              $('#tree').treeview('expandNode', [ itsParent.nodeId, {  } ]);
-              this.send('expandParent', itsParent.nodeId);
-          }
-        }
-    },
-    revealNode: function(nodeId){
-        $('#tree').treeview('revealNode', [ nodeId, { silent: true } ]);
     }
-
   }
 });
